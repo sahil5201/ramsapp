f97dba76e68c59c0c2e6d3b24f7005eeeb9266d9
Sahil Patel <sahil5555patel@gmail.com>
2020-05-27 22:21:16 +0530

    Updates

---
+++ precache-manifest.6ebcd925c99dad56d5ceac3c7aa8b4c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63d3a73961c2edff9d44e5051793ac30",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6752660edaa4c70f0d8d",
+    "url": "/static/css/main.4f24d775.chunk.css"
+  },
+  {
+    "revision": "e2a2fd95dad388105918",
+    "url": "/static/js/2.ed8941f6.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/static/js/2.ed8941f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6752660edaa4c70f0d8d",
+    "url": "/static/js/main.8c681821.chunk.js"
+  },
+  {
+    "revision": "4305ad129dcf89844c80",
+    "url": "/static/js/runtime-main.3ea18b6e.js"
+  }
+]);
